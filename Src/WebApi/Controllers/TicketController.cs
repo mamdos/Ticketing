ab6c1523c148be8caa1d078a8c749baea89b19e0
Mohammad Hossein <mhosein8080@gmail.com>
2024-02-05 22:49:04 +0330

    add ticket controller and its endpoints

---
+++ Src/WebApi/Controllers/TicketController.cs
@@ -0,0 +1,35 @@
+﻿using Data.Entities.User.Aggregate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Authorize(Roles = UserRoles.IssuerUser)]
+[Route("api/[controller]")]
+public class TicketController : ControllerBase
+{
+    [HttpGet("MyTickets")]
+    public async Task<IActionResult> MyTickets()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("NewTicket")]
+    public async Task<IActionResult> New()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("EditTicket")]
+    public async Task<IActionResult> Edit()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("DeleteTicket")]
+    public async Task<IActionResult> Delete()
+    {
+        throw new NotImplementedException();
+    }
+}
