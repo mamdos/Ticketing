572289c90cede4f76aadae2f5895fdc405071185
Mohammad Hossein <mhosein8080@gmail.com>
2024-02-07 01:08:05 +0330

    add category and category management controllers

---
+++ Src/WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Data.Entities.User.Aggregate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services.Category;
+using WebApi.Models;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Authorize(Roles = $"{UserRoles.CategoryAdmin},{UserRoles.TicketUser}")]
+[Route("api/[controller]")]
+public class CategoryController : ControllerBase
+{
+    private readonly ICategoryManager _categoryManager;
+
+    public CategoryController(ICategoryManager categoryManager)
+    {
+        _categoryManager = categoryManager;
+    }
+
+    [HttpGet("Categories")]
+    public async Task<IActionResult> Categories()
+    {
+        var result = await _categoryManager.GetCategoriesAsync(HttpContext.RequestAborted);
+
+        if (result.IsSucceed)
+            return Ok(ApiResponse.Successful(result.Data));
+
+        return BadRequest(ApiResponse.Fail(result.Message));
+    }
+}
---
+++ Src/WebApi/Controllers/CategoryManagementController.cs
@@ -0,0 +1,54 @@
+﻿using Data.Entities.User.Aggregate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services.Category;
+using Services.Category.Dto;
+using WebApi.Models;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Authorize(Roles = UserRoles.CategoryAdmin)]
+[Route("api/Admin/[controller]")]
+public class CategoryManagementController : ControllerBase
+{
+    private readonly ICategoryManager _categoryManager;
+
+    public CategoryManagementController(ICategoryManager categoryManager)
+    {
+        _categoryManager = categoryManager;
+    }
+
+    [HttpPost("AddCategory")]
+    public async Task<IActionResult> Add([FromBody] CreateCategoryRequestDto createCategoryRequestDto)
+    {
+        var result = await _categoryManager.CreateAsync(createCategoryRequestDto, HttpContext.RequestAborted);
+
+        if (result.IsSucceed)
+            return Ok(ApiResponse.Successful());
+
+        return BadRequest(ApiResponse.Fail(message: result.Message));
+    }
+
+    [HttpPut("EditCategory")]
+    public async Task<IActionResult> Edit([FromBody] UpdateCategoryRequestDto updateCategoryRequestDto)
+    {
+        var result = await _categoryManager.UpdateAsync(updateCategoryRequestDto, HttpContext.RequestAborted);
+
+        if (result.IsSucceed)
+            return Ok(ApiResponse.Successful());
+
+        return BadRequest(ApiResponse.Fail(message: result.Message));
+    }
+
+    [HttpDelete("DeleteCategory")]
+    public async Task<IActionResult> Delete([FromBody] DeleteCategoryReqeustDto deleteCategoryReqeustDto)
+    {
+        var result = await _categoryManager.DeleteAsync(deleteCategoryReqeustDto, HttpContext.RequestAborted);
+
+        if (result.IsSucceed)
+            return Ok(ApiResponse.Successful());
+
+        return BadRequest(ApiResponse.Fail(message: result.Message));
+    }
+}
