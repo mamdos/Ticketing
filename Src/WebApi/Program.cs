53acb50b24228b120446f5040952df7fca242961
Mohammad Hossein <mhosein8080@gmail.com>
2024-02-07 01:08:34 +0330

    add seeding on project start

--- Src/WebApi/Program.cs
+++ Src/WebApi/Program.cs
@@ -1,5 +1,9 @@
 using Data.Common.IoC;
+using Data.Persistence.DbContexts;
+using Data.Persistence.Seed;
+using Microsoft.EntityFrameworkCore;
 using Services.Common.IoC;
+using Services.Common.Models;
 using WebApi.Common.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDataLayer();
-builder.Services.AddServicesLayer();
+builder.Services.AddServicesLayer(builder.Configuration);
 builder.Services.AddApiLayer(builder.Configuration);
 
 builder.Services.AddControllers();
@@ -16,6 +20,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+
+    context.Database.Migrate();
+
+    var seedUserPassword = builder.Configuration.GetValue<string>("SeedSuperUserPassword");
+
+    await UserSeeder.Initialize(services, seedUserPassword ?? "");
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
