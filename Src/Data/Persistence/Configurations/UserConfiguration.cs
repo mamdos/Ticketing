efc0de4c61a7915b46960ac3934b614c9f0850e0
Mohammad Hossein <mhosein8080@gmail.com>
2024-02-07 01:04:44 +0330

    add first name and last name to user

---
+++ Src/Data/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Data.Entities.User.Aggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(x => x.FirstName).HasMaxLength(100);
+        builder.Property(x => x.LastName).HasMaxLength(100);
+    }
+}
--- Src/Data/Persistence/Migrations/20240206203423_add_name_to_user.Designer.cs
+++ Src/Data/Persistence/Migrations/20240206203423_add_name_to_user.Designer.cs
@@ -4,6 +4,7 @@ using Data.Persistence.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240206203423_add_name_to_user")]
+    partial class add_name_to_user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
